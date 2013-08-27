9cefc53c4d846f33ffcf84ad0c140906504bdbca
Breezewish <me@breeswish.org>
2013-08-27 15:40:24 +0800

    implement repository/create

--- app.rb
+++ app.rb
@@ -1,12 +1,23 @@
 require 'bundler'
 require 'grape'
+require 'rugged'
 
 class OpenVJGitAPI < Grape::API
 
     format :json
 
-    get ':hello' do
-        {hello: 'world'}
+    get :ping do
+        {pong: Time.now.to_i}
+    end
+
+    resource :repository do
+
+        desc 'Create a new repository'
+        post :create do
+            ret = Rugged::Repository.init_at(params[:path], :bare)
+            {result: ret}
+        end
+
     end
     
 end
